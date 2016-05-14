f5d32213d20f26224593981d00bff8531c300e4b
poodarchu <poodarchu@gmail.com>
2016-05-15 01:51:12 +0800

    Revert "Merge branch 'elaboration1'"
    This reverts commit e8ffb12656485c7ed1c993427277d8e7cdfdc059, reversing
    changes made to 9ae114ce0f4ced0d39b920e81414e7ab013a513b.

--- QueryPro/core/search.h
+++
@@ -1,8 +0,0 @@
-#ifndef __SEARCH_H__
-#define __SEARCH_H__
-
-#include "gitsoo.h"
-
-void search(gitsoo_env *env, const char *query);
-
-#endif
--- SpiderPro/Spider_jar/Readme.md
+++
@@ -1,2 +0,0 @@
-##run:java -jar Spider.jar
-##需将配置文件Spider.conf和stopwords.txt与Spider放置同一目录下
